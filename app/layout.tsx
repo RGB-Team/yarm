fb457d9bf1b967ed1d88223f508fb6ec3b7b22f3
gsdev <gaurangrshah@gmail.com>
2024-11-29 16:04:25 -0500

    feat:dev: tailwind responsive indicator

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import { ThemeToggle } from "@/components/theme-toggle"
 import { constructMetadata } from "@/lib/metadata"
+import { TailwindIndicator } from "@/components/tw-indicator"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,6 +37,7 @@ export default function RootLayout({
         >
           {children}
           <ThemeToggle />
+          <TailwindIndicator />
         </ThemeProvider>
       </body>
     </html>
