a6f9afd5232a69bb754dd9e617b5d14182988be1
gaurangrshah <gaurang.r.shah@gmail.com>
2025-01-03 19:53:38 -0500

    refactor (imports) - resolve shadcn-ui imports

--- apps/web/components/icons/github.tsx
+++ apps/web/components/icons/github.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@yarm/ui/lib/utils"
 
 type GithubIconProps = {
     className?: string
--- apps/web/components/icons/sheet.tsx
+++ apps/web/components/icons/sheet.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@yarm/ui/lib/utils"
 
 type SheetIconProps = {
     className?: string
